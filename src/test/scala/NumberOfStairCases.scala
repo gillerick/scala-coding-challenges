53144671bb2894541644b5ca3e5c4d06cd66ca01
Gill Erick <ogayogill95@gmail.com>
2023-04-30 12:10:45 +0300

    Implement dp solutiion and tests for numberOfStairCases problem

--- src/test/scala/NumberOfStairCases.scala
+++ src/test/scala/NumberOfStairCases.scala
@@ -1,11 +1,18 @@
 import munit.FunSuite
 import NumberOfStairCases.RecursiveSolution
+import NumberOfStairCases.DpSolution
 
 class NumberOfStairCases extends FunSuite {
   test("number of stair cases - recursive solution") {
     assertEquals(RecursiveSolution.numberOfStairCases(1), 1)
     assertEquals(RecursiveSolution.numberOfStairCases(3), 4)
     assertEquals(RecursiveSolution.numberOfStairCases(10), 274)
+  }
+
+  test("number of stair cases - dp solution") {
+    assertEquals(DpSolution.numberOfStairCases(1), 1)
+    assertEquals(DpSolution.numberOfStairCases(3), 4)
+    assertEquals(DpSolution.numberOfStairCases(10), 274)
   }
 
 }
