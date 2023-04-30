2b50dd7ec3fd32f7077d1f7109c45ea6ad344cad
Gill Erick <ogayogill95@gmail.com>
2023-04-30 10:40:11 +0300

    Add unit tests for ZeroSumSubArray problem

---
+++ src/test/scala/ZeroSumSubArraySuite.scala
@@ -0,0 +1,25 @@
+import munit.FunSuite
+import zeroSumSubArray.zeroSumSubArray
+
+class ZeroSumSubArraySuite extends FunSuite {
+  test("An empty subarray should return false") {
+    assert(!zeroSumSubArray.isZeroSumSubArray(List.empty[Int]))
+  }
+
+  test("An array comprising only zeros should return true") {
+    assert(zeroSumSubArray.isZeroSumSubArray(List(0)))
+    assert(zeroSumSubArray.isZeroSumSubArray(List(0, 0, 0)))
+  }
+
+  test("An array with no subarray adding to zero") {
+    assert(!zeroSumSubArray.isZeroSumSubArray(List(1, 1, 1)))
+    assert(!zeroSumSubArray.isZeroSumSubArray(List(-1, -1, -1)))
+    assert(!zeroSumSubArray.isZeroSumSubArray(List(1, 2, 3)))
+  }
+
+  test("An array with subarray adding to zero") {
+    assert(zeroSumSubArray.isZeroSumSubArray(List(1, 2, 3, 4, 0, 5, 6, 7)))
+    assert(zeroSumSubArray.isZeroSumSubArray(List(2, -2)))
+  }
+
+}
