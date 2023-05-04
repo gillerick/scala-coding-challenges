c29b15f8e4f65ccd8be21ba6fa43519dc96ecac6
Gill Erick <ogayogill95@gmail.com>
2023-05-04 20:51:25 +0300

    Add functional solution and test for SmallestDifferencePair problem

--- src/main/scala/algorithms/SmallestDifference.scala
+++ src/main/scala/algorithms/SmallestDifference.scala
@@ -1,5 +1,7 @@
 package algorithms
 
+import java.lang.Math.abs
+
 object SmallestDifference {
   def smallestDifferenceTwoPointer(arrayOne: Array[Int], arrayTwo: Array[Int]): Array[Int] = {
     arrayOne.sortInPlace()
@@ -14,7 +16,7 @@ object SmallestDifference {
     while (indexOne < arrayOne.length && indexTwo < arrayTwo.length) {
       val first = arrayOne(indexOne)
       val second = arrayTwo(indexTwo)
-      val current = Math.abs(first - second)
+      val current = abs(first - second)
 
       if (current < smallest) {
         smallest = current
@@ -28,6 +30,20 @@ object SmallestDifference {
       }
     }
     smallestPair
+  }
+
+  def smallestDifferenceFunctional(arrayOne: Array[Int], arrayTwo: Array[Int]): Array[Int] = {
+    val sortedArrayOne = arrayOne.sorted
+    val sortedArrayTwo = arrayTwo.sorted
+
+    val smallestPair = sortedArrayOne.
+      flatMap { first =>
+        sortedArrayTwo.map {
+          second =>
+            (first, second, abs(first - second))
+        }
+      }.minBy(_._3)
+    Array(smallestPair._1, smallestPair._2)
   }
 
 }
--- src/test/scala/SmallestDifferenceSuite.scala
+++ src/test/scala/SmallestDifferenceSuite.scala
@@ -1,10 +1,14 @@
-import algorithms.SmallestDifference.smallestDifferenceTwoPointer
+import algorithms.SmallestDifference.{smallestDifferenceFunctional, smallestDifferenceTwoPointer}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers._
 
 class SmallestDifferenceSuite extends AnyFlatSpec {
-  "smallestDifference" should "correctly return the minimum difference between the array elements" in {
+  "smallestDifference" should "correctly return the pair with the minimum difference for a given array" in {
     smallestDifferenceTwoPointer(Array(-1, 5, 10, 20, 28, 3), Array(26, 134, 135, 15, 17)) should be(Array(28, 26))
+  }
+
+  "smallestDifferenceFunctional" should "correctly return the pair with the minimum difference for a given array" in {
+    smallestDifferenceFunctional(Array(-1, 5, 10, 20, 28, 3), Array(26, 134, 135, 15, 17)) should be(Array(28, 26))
   }
 
 }
