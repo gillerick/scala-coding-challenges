8831daec8ccf9f323ba1b613f0376d21a46e8e36
Gill Erick <ogayogill95@gmail.com>
2023-05-04 18:29:33 +0300

    Add functional solution for FirstNonRepeatingCharacter problem

--- src/main/scala/algorithms/FirstNonRepeatingCharacter.scala
+++ src/main/scala/algorithms/FirstNonRepeatingCharacter.scala
@@ -10,4 +10,21 @@ object FirstNonRepeatingCharacter {
     -1
   }
 
+  /*
+  This code first creates a map of character frequencies using the groupBy and mapValues methods.
+  It then iterates over each character in the input string using the foldLeft method.
+  If the current character has a frequency of 1 and no previous non-repeating character has been found,
+  it returns the index of that character using the indexOf method. Otherwise, it returns the previous result
+  */
+  def firstNonRepeatingCharacterFunctional(stringInput: String): Int = {
+    val frequencies = stringInput.groupBy(identity).view.mapValues(_.length)
+    stringInput.foldLeft(-1)((index, character) => {
+      if (frequencies(character) == 1 && index == -1) {
+        stringInput.indexOf(character)
+      } else {
+        index
+      }
+    })
+  }
+
 }
--- src/test/scala/FirstNonRepeatingCharacter.scala
+++ src/test/scala/FirstNonRepeatingCharacter.scala
@@ -1,15 +1,23 @@
 import org.scalatest.flatspec.AnyFlatSpec
-import algorithms.FirstNonRepeatingCharacter.firstNonRepeatingCharacter
+import algorithms.FirstNonRepeatingCharacter.{firstNonRepeatingCharacter, firstNonRepeatingCharacterFunctional}
 import org.scalatest.matchers.should.Matchers._
 
 class FirstNonRepeatingCharacter extends AnyFlatSpec {
-  "FirstNonRepeatingCharacter" should "correctly return the first non" in {
+  "FirstNonRepeatingCharacter" should "correctly return the first non repeating character in a string input" in {
     firstNonRepeatingCharacter("abcdcaf") should be(1)
     firstNonRepeatingCharacter("a") should be(0)
     firstNonRepeatingCharacter("abac") should be(1)
     firstNonRepeatingCharacter("faadabcbbebdf") should be(6)
     firstNonRepeatingCharacter("lmnopqldsafmnopqsa") should be(7)
 
+  }
+
+  "firstNonRepeatingCharacterHashMap" should "correctly return the first non repeating character in a string input" in {
+    firstNonRepeatingCharacterFunctional("abcdcaf") should be(1)
+    firstNonRepeatingCharacterFunctional("a") should be(0)
+    firstNonRepeatingCharacterFunctional("abac") should be(1)
+    firstNonRepeatingCharacterFunctional("faadabcbbebdf") should be(6)
+    firstNonRepeatingCharacterFunctional("lmnopqldsafmnopqsa") should be(7)
   }
 
 }
