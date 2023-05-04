53e541c750480790591e3c65925059146e051547
Gill Erick <ogayogill95@gmail.com>
2023-05-04 19:23:14 +0300

    Solve & test MoveElementToEnd using the two pointer approach

--- src/main/scala/algorithms/MoveElementToEnd.scala
+++ src/main/scala/algorithms/MoveElementToEnd.scala
@@ -9,4 +9,24 @@ object MoveElementToEnd {
     array.filter(_ != toMove) ++ array.filter(_ == toMove)
   }
 
+  def moveElementToEndTwoPointer(array: Array[Int], toMove: Int): Array[Int] = {
+    var leftIdx = 0
+    var rightIdx = array.length - 1
+    while (leftIdx < rightIdx) {
+      val leftNum = array(leftIdx)
+      val rightNum = array(rightIdx)
+      if (leftNum == toMove && rightNum != toMove) {
+        array(leftIdx) = rightNum
+        array(rightIdx) = leftNum
+        leftIdx += 1
+        rightIdx -= 1
+      } else if (rightNum == toMove) {
+        rightIdx -= 1
+      } else if (leftNum != toMove) {
+        leftIdx += 1
+      }
+    }
+    array
+  }
+
 }
--- src/test/scala/MoveElementToEndSuite.scala
+++ src/test/scala/MoveElementToEndSuite.scala
@@ -1,4 +1,4 @@
-import algorithms.MoveElementToEnd.moveElementToEndInPlace
+import algorithms.MoveElementToEnd.{moveElementToEndInPlace, moveElementToEndTwoPointer}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers._
 
@@ -8,6 +8,13 @@ class MoveElementToEndSuite extends AnyFlatSpec {
     moveElementToEndInPlace(List.empty[Int], 6) should be(List.empty[Int])
     moveElementToEndInPlace(List(3, 3, 3, 3, 3, 3), 3) should be(List(3, 3, 3, 3, 3, 3))
     moveElementToEndInPlace(List(3, 1, 2, 4, 5), 3) should be(List(1, 2, 4, 5, 3))
+  }
+
+  "moveElementToEndTwoPointer" should "correctly move the specified element to the end of the array" in {
+    moveElementToEndTwoPointer(Array(2, 1, 2, 2, 2, 3, 4, 2), 2) should be(List(4, 1, 3, 2, 2, 2, 2, 2))
+    moveElementToEndTwoPointer(Array.empty[Int], 6) should be(List.empty[Int])
+    moveElementToEndTwoPointer(Array(3, 3, 3, 3, 3, 3), 3) should be(List(3, 3, 3, 3, 3, 3))
+    moveElementToEndTwoPointer(Array(3, 1, 2, 4, 5), 3) should be(List(5, 1, 2, 4, 3))
   }
 
 }
